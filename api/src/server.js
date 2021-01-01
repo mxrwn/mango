a52d169ed4355f32465a6b6b029dc8b9b4bd47d9
MarwaneElKchiri <marwane.el.kchiri@student.ehb.be>
2021-01-01 13:45:14 +0100

    init app

---
+++ api/src/server.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const morgan = require('morgan')
+const helmet = require('helmet')
+
+const app = express()
+
+app.use(morgan('dev'))
+.use(helmet())
+
+app.get('/', (req, res) => {
+
+  res.json({
+    message : 'Welcome to the mongo API !'
+  })
+
+})
+
+module.exports = app
