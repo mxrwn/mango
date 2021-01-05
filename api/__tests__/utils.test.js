13a6402115fc095ac840bab356f1b71868aa4342
MarwaneElKchiri <marwane.el.kchiri@student.ehb.be>
2021-01-05 14:44:25 +0100

    /POST & /GET tests

--- api/__tests__/utils.test.js
+++ api/__tests__/utils.test.js
@@ -1,8 +1,26 @@
 const { GetMangasFromScanTrad } = require('./../src/utils/utils')
-
-test('Should output all scantrad mangas', () => {
+const request = require('supertest')
+const app = require('./../src/server')
+test('GetMangasFromScanTrad Should go well', () => {
   const result = GetMangasFromScanTrad()
   result.then(data => {
     expect(data).toBe(200)
   })
+})
+
+test('GET /manga should give all mangas saved in database', () => {
+  request(app).get('/manga').expect(200)
+})
+
+test('POST /manga should add a manga', () => {
+  request(app).post('/manga')
+  .send({
+    title : 'test',
+    url : 'test.com',
+    image : 'test',
+    total : 100,
+    scantrad : false,
+    category : 'all'
+  })
+  .expect(200)
 })
